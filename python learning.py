d29169f14e924b3f16666f73b1ac6a544259fe7f
Gautampareek96 <gautampareek29@gmail.com>
2026-06-02 16:12:06 +0530

    Day 1 & 2: variables, f-strings, input(), operators

--- python learning.py
+++ python learning.py
@@ -1,9 +1,11 @@
-name="Gautam"
-age=21
-salary=20000
+name = input("What is your name?  :")
+age = int(input("What is your age?  :"))
+current_year = int(input ("What is the current year?  :"))
+future_year = int(input("What year do yuou want to know your age in?  :"))
+salary=500000
 is_employed=True
 city="Pune"
-print(name)
-print(age)
-print(salary)
-print(is_employed)
+
+#  print(f"My name is {name} and I am {age}. I am earning {salary} every month.")
+print(f"{name} will be {age + future_year-current_year} years old in {future_year}.")
+
