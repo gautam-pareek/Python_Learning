aab7aa1f021fa0793f70f3dda086736e0e3975d7
Gautampareek96 <gautampareek29@gmail.com>
2026-06-02 17:32:22 +0530

    Day 2: even/odd, squared and division with remainder & quotient

---
+++ program for odd or even.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a number:"))
+
+if number%2==0:
+    print("Even")
+else:
+    print("odd")
+
+print("the number squared will be",number**2)
+print(f"the number division with 7 will be remainder {number % 7} & quotient will be {number // 7}")
